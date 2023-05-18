5fdb173d0f74931143cc5d49055777898d2eecd1
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-05-18 13:11:39 +0700

    feat: initial commit

---
+++ client-side/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/NavbarCustom.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
